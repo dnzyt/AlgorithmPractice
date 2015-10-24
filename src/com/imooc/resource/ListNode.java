1035ab04f7f6abc99968f9e89895a17bae489eb2
dnzyt <dnzyt1986@gmail.com>
2015-10-24 19:44:09 -0400

    ListNode

---
+++ src/com/imooc/resource/ListNode.java
@@ -0,0 +1,13 @@
+package com.imooc.resource;
+
+/**
+ * Created by dnzyt on 10/24/2015.
+ */
+public class ListNode {
+    public int val;
+public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
