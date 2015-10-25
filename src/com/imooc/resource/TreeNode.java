6e2846d9eaf6d30b10086278c93b083151bd666a
dnzyt <dnzyt1986@gmail.com>
2015-10-24 23:05:17 -0400

    TreeNode

---
+++ src/com/imooc/resource/TreeNode.java
@@ -0,0 +1,19 @@
+package com.imooc.resource;
+
+/**
+ * Created by dnzyt on 10/24/2015.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+
+
+}
