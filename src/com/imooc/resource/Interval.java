e58712d665928c91b6b399ee33fe3285fc9499f5
dnzyt <dnzyt1986@gmail.com>
2015-10-31 12:19:42 -0400

    Interval

---
+++ src/com/imooc/resource/Interval.java
@@ -0,0 +1,15 @@
+package com.imooc.resource;
+
+/**
+ * Created by dnzyt on 10/27/2015.
+ */
+public class Interval {
+    public int start;
+    public int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+}
