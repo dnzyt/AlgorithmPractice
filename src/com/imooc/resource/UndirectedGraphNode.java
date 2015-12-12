8cb95d593c972f9f9f4056b7c7a42982b00ff67a
dnzyt <dnzyt1986@gmail.com>
2015-12-11 22:30:41 -0500

    UndirectedGraphNode public constructor

--- src/com/imooc/resource/UndirectedGraphNode.java
+++ src/com/imooc/resource/UndirectedGraphNode.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class UndirectedGraphNode {
     public int label;
     public ArrayList<UndirectedGraphNode> neighbors;
-    UndirectedGraphNode(int x) {
+    public UndirectedGraphNode(int x) {
         label = x;
         neighbors = new ArrayList<>();
     }
