f243631b034b57f8b4fc23abd4f9a3efc1e278e8
dnzyt <dnzyt1986@gmail.com>
2015-12-20 12:50:39 -0500

    RandomListNode

--- src/com/imooc/resource/RandomListNode.java
+++ src/com/imooc/resource/RandomListNode.java
@@ -4,11 +4,11 @@ package com.imooc.resource;
  * Created by dnzyt on 12/20/2015.
  */
 public class RandomListNode {
-    public int lable;
+    public int label;
     public RandomListNode next;
     public RandomListNode random;
     public RandomListNode(int x) {
-        this.lable = x;
+        this.label = x;
         random = null;
         next = null;
     }
