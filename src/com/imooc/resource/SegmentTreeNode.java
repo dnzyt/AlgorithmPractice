ad3f91c06ea424fa03fab0a201a43d36c4c5e539
dnzyt <dnzyt1986@gmail.com>
2015-12-20 22:40:55 -0500

    Combination Sum

--- src/com/imooc/resource/SegmentTreeNode.java
+++ src/com/imooc/resource/SegmentTreeNode.java
@@ -7,11 +7,20 @@ public class SegmentTreeNode {
 
     public int start;
     public int end;
+    public int count;
     public SegmentTreeNode left;
     public SegmentTreeNode right;
     public SegmentTreeNode(int start, int end) {
         this.start = start;
         this.end = end;
+        this.left = null;
+        this.right = null;
+    }
+
+    public SegmentTreeNode(int start, int end, int count) {
+        this.start = start;
+        this.end = end;
+        this.count = count;
         this.left = null;
         this.right = null;
     }
